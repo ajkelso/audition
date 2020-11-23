5c3a5d461fbaf064fbdc06b3cded9288e9e71a28
ajkelso <ajkelso@gmail.com>
2020-11-23 14:39:17 -0500

    create form and actions for Actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,49 @@
 class ActorsController < ApplicationController
+    before_action :find_actor, only: [:show, :edit, :update]
+
+    def index
+        @actors = Actor.all 
+    end
+    
+    def show
+    end
+
+    def new
+        @actor = Actor.new
+    end
+
+    def create
+        
+
+        @actor = Actor.new(actor_params)
+        if @actor.save
+            session[:actor_id] = actor.id 
+            redirect_to actor_path(actor)
+        else
+            byebug
+            render 'new'
+        end
+    end
+
+    def edit
+    
+    end
+
+    def update
+    
+    end
+
+
+
+
+
+    private
+
+    def find_actor
+        @actor = Actor.find_by(params[:id])
+    end
+
+    def actor_params
+        params.require(:actor).permit(:name, :email, :phone_number, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :casting_directors
   resources :projects
   resources :auditions
-  resources :actors
+  resources :actors, exclude: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
