586746fe3fbdbad8deba8efc885c30f8385066a5
ajkelso <ajkelso@gmail.com>
2020-11-24 14:44:55 -0500

    add edit link to show, create #current_user in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        if session[:actor_id]
+            Actor.find_by(id: session[:actor_id])
+        elsif session[:director_id]
+            Director.find_by(id: session[:director_id])
+        elsif session[:casting_director_id]
+            CastingDirector.find_by(id: session[:casting_director_id])
+        end
+    end
 end
