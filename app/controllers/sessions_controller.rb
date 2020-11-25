1e6b99a6dcdab50ba7dbee39c6487b2b528d37ec
ajkelso <ajkelso@gmail.com>
2020-11-24 21:24:28 -0500

    build sessions #create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,16 @@ class SessionsController < ApplicationController
     def new_actor
     end
 
-    
+    def create
+
+        @user = params[:user_type].constantize.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[params[:user_type].to_sym] = @user.id
+            redirect_to "/#{params[:user_type].downcase}s/#{@user.id}"
+        else
+            flash[:errors] = "Invalid email address or password"
+            redirect_to new_session_path
+        end
+    end
     
 end
