bc8b8dad5932b33cc64030cb076f6c572a3c315b
ajkelso <ajkelso@gmail.com>
2020-11-25 12:20:36 -0500

    build sessions@destroy & add Log Out link to actor_show_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionsController < ApplicationController
-    def new_actor
+    def new
     end
 
     def create
         @user = params[:user_type].constantize.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
             session["#{params[:user_type].downcase}_id".to_sym] = @user.id
-            byebug
             redirect_to "/#{params[:user_type].downcase}s/#{@user.id}"
         else
             flash[:errors] = "Invalid email address or password"
             redirect_to new_session_path
         end
+    end
+
+    def destroy
+        [:actor_id, :director_id, :castingdirector_id].each { |id| session.delete(id) } 
+        byebug
+        redirect_to new_session_path
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :directors
-  resources :casting_directors
-  resources :projects
-  resources :auditions
-  resources :actors, exclude: [:destroy]
+  resources :directors, exclude: [:destroy]
+  resources :casting_directors, exclude: [:destroy]
+  resources :actors, exclude: [:destroy] do
+    resources :auditions, shallow: true
+  end
+  resources :projects, exclude: [:destroy] do
+    resources :auditions, shallow: true
+  end
+  # resources :auditions
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
