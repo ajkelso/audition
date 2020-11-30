0fc6aa4738eb969b50458c243a881d1b7616a84a
ajkelso <ajkelso@gmail.com>
2020-11-30 15:23:18 -0500

    build audition#index + view

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -1,15 +1,29 @@
 class AuditionsController < ApplicationController
     before_action :find_audition, only: [:show, :edit, :update, :destroy]
-    def new
-        if params[:actor_id] && !Actor.exists?(params[:actor_id])
+    
+    def index
+        if params[:actor_id]
+            @actor = Actor.find_by(id: params[:actor_id])
+            if @actor
+                @auditions = @actor.auditions
+            else
                 flash[:error] = "Actor not found."
-            redirect_to actors_path
-        elsif params[:project_id] && !Project.exists?(params[:project_id])
-            flash[:error] = "Project not found."
-            redirect_to projects_path
-        else  
-            @audition = Audition.new(actor_id: params[:actor_id], project_id: params[:project_id])
+                redirect_to actors_path
+            end
+        elsif params[:project_id]
+            @project = Project.find_by(id: params[:project_id])
+            if @project 
+                @auditions = @project.auditions 
+            else
+                flash[:error] = "Project not found."
+                redirect_to projects_path
+            end
         end
+    end
+    
+    def new
+        verify_actor_or_project  
+        @audition = Audition.new(actor_id: params[:actor_id], project_id: params[:project_id])
     end
 
     def create
@@ -31,6 +45,16 @@ class AuditionsController < ApplicationController
 
     def find_audition
         @audition = Audition.find_by(id: params[:id])
+    end
+
+    def verify_actor_or_project
+        if params[:actor_id] && !Actor.exists?(params[:actor_id])
+            flash[:error] = "Actor not found."
+            redirect_to actors_path
+        elsif params[:project_id] && !Project.exists?(params[:project_id])
+            flash[:error] = "Project not found."
+            redirect_to projects_path
+        end
     end
     
 
