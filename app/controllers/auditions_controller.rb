a44e6c65442889b48cacd7629f6e5faddf90f548
ajkelso <ajkelso@gmail.com>
2020-12-01 14:29:43 -0500

    move error messages to layout; verify actor or project in auditions#new

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -2,20 +2,21 @@ class AuditionsController < ApplicationController
     before_action :find_audition, only: [:show, :edit, :update, :destroy]
     
     def index
+        
         if params[:actor_id]
             @actor = Actor.find_by(id: params[:actor_id])
-            if @actor
+            if owner?
                 @auditions = @actor.auditions
             else
-                flash[:error] = "Actor not found."
+                flash[:error] = "You do not have access"
                 redirect_to actors_path
             end
         elsif params[:project_id]
             @project = Project.find_by(id: params[:project_id])
-            if @project 
+            if @project && current_user == @project.director || @project.casting_director
                 @auditions = @project.auditions 
             else
-                flash[:error] = "Project not found."
+                flash[:error] = "You do not have access"
                 redirect_to projects_path
             end
         end
@@ -36,7 +37,6 @@ class AuditionsController < ApplicationController
         end
     end
 
-
     private
 
     def audition_params
@@ -48,14 +48,16 @@ class AuditionsController < ApplicationController
     end
 
     def verify_actor_or_project
-        if params[:actor_id] && !Actor.exists?(params[:actor_id])
-            flash[:error] = "Actor not found."
+        if params[:actor_id] && params[:actor_id].to_i != session[:actor_id]
+            flash[:error] = "You cannot create or modify an audition for another actor."
             redirect_to actors_path
-        elsif params[:project_id] && !Project.exists?(params[:project_id])
-            flash[:error] = "Project not found."
-            redirect_to projects_path
+        elsif params[:project_id]
+            project = Project.find_by(params[:project_id])
+            if project && current_user != project.director || project.casting_director 
+                flash[:error] = "Project not found."
+                redirect_to projects_path
+            end
         end
     end
-    
 
 end
