15c760c1fbc7570e1097101be8323351a1f29c07
ajkelso <ajkelso@gmail.com>
2020-12-04 14:51:07 -0500

    create and abstract sessions#create_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     
-    skip_before_action :verified_user, only: [:new, :create, :google_login, :signup, :google_signup]
+    skip_before_action :verified_user, only: [:new, :create, :google_login, :signup, :google_signup, :create_user]
     
     def new
         signed_in?
@@ -18,6 +18,18 @@ class SessionsController < ApplicationController
     end
 
     def signup
+        
+    end
+
+    def create_user
+        @user = params[:user_type].constantize.new(session_params)
+        if @user.save
+            session["#{params[:user_type].downcase}_id".to_sym] = @user.id
+            redirect_to "/#{params[:user_type].downcase}s/#{@user.id}/profile"
+        else    
+            flash.now[:errors] = @user.errors.full_messages
+            render :signup
+        end     
     end
     
     def google_signup
@@ -39,6 +51,12 @@ class SessionsController < ApplicationController
     def destroy
         [:actor_id, :director_id, :castingdirector_id].each { |id| session.delete(id) } 
         redirect_to new_session_path
+    end
+
+    private
+
+    def session_params
+        params.permit(:name, :email, :phone_number, :password, :password_confirmation)
     end
 
     
