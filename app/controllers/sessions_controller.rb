96aeb912d488877b92fb0142f57b9b43f246f8c2
ajkelso <ajkelso@gmail.com>
2020-12-04 14:29:23 -0500

    create signup session view & modify google sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
     
-    skip_before_action :verified_user, only: [:new, :create, :google_login]
+    skip_before_action :verified_user, only: [:new, :create, :google_login, :signup, :google_signup]
     
     def new
         signed_in?
@@ -17,15 +17,23 @@ class SessionsController < ApplicationController
         end
     end
 
+    def signup
+    end
+    
+    def google_signup
+        session[:user_type] = params[:user_type]
+        redirect_to '/auth/google_oauth2'
+    end
+
     def google_login
         name = request.env['omniauth.auth']['info']['name']
         email = request.env['omniauth.auth']['info']['email']
-        @actor = Actor.find_or_create_by(email: email) do |actor|
-            actor.name = name
-            actor.password = SecureRandom.hex
+        @user = session[:user_type].constantize.find_or_create_by(email: email) do |user|
+            user.name = name
+            user.password = SecureRandom.hex
         end
-        session[:actor_id] = @actor.id
-        redirect_to actor_profile_path(@actor)
+        session["#{session[:user_type].downcase}_id".to_sym] = @user.id
+        redirect_to actor_profile_path(@user)
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,10 @@ Rails.application.routes.draw do
   get '/actors/:id/profile', to: 'actors#profile', as: 'actor_profile'
 
   get '/auth/google_oauth2/callback', to: 'sessions#google_login'
+  post '/google_signup', to: 'sessions#google_signup'
+
+  get '/signup', to: 'sessions#signup'
+  post '/signup', to: 'sessions#create_user'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
