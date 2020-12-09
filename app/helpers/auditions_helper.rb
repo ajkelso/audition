1d9b7482946188885b91842f7cece40beefb9eb7
ajkelso <ajkelso@gmail.com>
2020-12-09 15:47:03 -0500

    change audition form styling

--- app/helpers/auditions_helper.rb
+++ app/helpers/auditions_helper.rb
@@ -2,7 +2,7 @@ module AuditionsHelper
     def actor_id_field(audition)
         concat label_tag "Actor: "
         if audition.actor.nil?
-            select_tag 'audition[actor_id]', options_from_collection_for_select(Actor.all, :id, :name)
+            select_tag 'audition[actor_id]', options_from_collection_for_select(Actor.all, :id, :name), class:"form-control"
         else
             concat audition.actor.name
             hidden_field_tag "audition[actor_id]", audition.actor_id
@@ -12,9 +12,9 @@ module AuditionsHelper
     def project_id_field(audition)
         concat label_tag "Project: " 
         if audition.project.nil? && creative?
-            select_tag 'audition[project_id]', options_from_collection_for_select(current_user.projects, :id, :title)
+            select_tag 'audition[project_id]', options_from_collection_for_select(current_user.projects, :id, :title), class:"form-control"
         elsif audition.project.nil? && !creative?
-            select_tag 'audition[project_id]', options_from_collection_for_select(Project.get_seeking, :id, :title)
+            select_tag 'audition[project_id]', options_from_collection_for_select(Project.get_seeking, :id, :title), class:"form-control"
         else
             concat audition.project.title
             hidden_field_tag "audition[project_id]", audition.project_id
