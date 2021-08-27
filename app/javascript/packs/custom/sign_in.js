363e40b5967d6750278fb7a0c1b603dbe6d3801d
ajkelso <ajkelso@gmail.com>
2021-08-27 15:10:49 -0400

    add hidden user input filed and func to change value upon user type selection

--- app/javascript/packs/custom/sign_in.js
+++ app/javascript/packs/custom/sign_in.js
@@ -1,5 +1,11 @@
 const userTypeInput = document.getElementById("user-type")
+const hiddenUserInput = document.querySelector(".hidden-user-type")
+
+const setUserType = (e) => {
+    console.log(e.target.value);
+    hiddenUserInput.value = e.target.value
+}
 // // add function to send value to second form
-userTypeInput.addEventListener('change', () => console.log(userTypeInput.value))
+userTypeInput.addEventListener('change', (e) => setUserType(e))
 
 console.log(userTypeInput)
