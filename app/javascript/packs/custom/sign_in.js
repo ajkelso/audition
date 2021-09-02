261a91764d014579666e74023d7c79adfe2248fb
ajkelso <ajkelso@gmail.com>
2021-09-01 22:06:42 -0400

    add js to require user_select

--- app/javascript/packs/custom/sign_in.js
+++ app/javascript/packs/custom/sign_in.js
@@ -1,9 +1,25 @@
 const userTypeInput = document.getElementById("user-type")
 const hiddenUserInput = document.querySelector(".hidden-user-type")
-const 
+const submitButtons = document.querySelectorAll(".sign-in")
+const userSelect = document.querySelector(".user-select")
+const form = document.querySelector(".google-form")
 
 const setUserType = (e) => {
     hiddenUserInput.value = e.target.value
 }
 
+const checkSelection = (e) => {
+    if(hiddenUserInput.value === ""){
+        e.preventDefault();
+        const errorDiv = document.createElement('div');
+        errorDiv.className += "text-center text-white bg-danger";
+        console.log(errorDiv)
+        errorDiv.innerHTML = "Please Select User Type";
+        form.insertBefore(errorDiv, userSelect)
+        // selectError.appendChild(errorDiv)
+        console.log("BOOM")
+    }
+}
+
 userTypeInput.addEventListener('change', (e) => setUserType(e))
+submitButtons.forEach( button => button.onmouseup = (e) => checkSelection(e))
